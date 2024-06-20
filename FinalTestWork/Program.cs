f1e2c62b9604377dde51cfcd13722e73d3aeab6e
Boris T <boristrakhon@gmail.com>
2024-06-21 00:51:59 +0300

    Step Three

--- FinalTestWork/Program.cs
+++ FinalTestWork/Program.cs
@@ -10,26 +10,13 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-// задаем массив 
-
-string[] strArray = new string[3];
-strArray = { "Hello", "2", "world", ":-)"};
-// Проверить, что выводит на печать 
-Console.WriteLine(strArray);
-
-
-
-// 1. На основе символов строки (тип string) 
-// 2. сформировать массив символов (тип char[]). 
-
-// Метод строки ToCharArray() можно использовать, но мы тут его не использовали, т.к. ещё не умеем.
-
-// Пример
-// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
 
 // Поле методов
 
-// создадим метод перевода строки в массив символов
+// Метод перевода строки в массив символов 
+// на основе символов строки (тип string) формируем массив символов (тип char[]). 
+// метод строки ToCharArray() можно использовать, но мы тут его не использовали, т.к. ещё не умеем.
+// по схеме “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
 char[] StringToCharArray(string s)
 {
     char[] strArray = new char[s.Length];
@@ -40,21 +27,8 @@ char[] StringToCharArray(string s)
     return strArray;
 }
 
-// получим строку от пользователя
-Console.WriteLine("Введите строку ");
-string str = Console.ReadLine();
-
-// вызываем метод преобразования из строки в массив символов
-char[] resArray = StringToCharArray(str);
-
-//Далее 
-// берём один элемент массива строк, уже переведённый в массив символов и проверяем количество символов в этой строке. Если количество символов в строке три, сохраняем в элемент нового массива 
-// (задать массив strThreeCharArray)
-if resArray.Lenght == 3
-	strThreeCharArray[i] = resArray
-
-// Пример цикла 1)
-void PrintArray(char[] array)
+// Метод печати массива строк
+void PrintArray(string[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
@@ -64,44 +38,51 @@ void PrintArray(char[] array)
         else
             Console.Write($"{array[i]}");
     }
-     Console.WriteLine("]");
+    Console.WriteLine("]");
 }
 
-//Пример цикла 2)
-int CountVowels(string str)
+// задаем массив 
+
+// получение массива строк от пользователя
+// ввод одной сторки
+// Console.WriteLine("Введите строку ");
+// string str = Console.ReadLine();
+
+string[] strArray = new string[4] { "Hello", "2", "world", ":-)" };
+
+// Проверить, что выводит на печать 
+// Console.WriteLine(strArray);
+
+// берем по порядку строки из массива строк
+int numThreeString = 0;
+for (int i = 0; i < strArray.Length; i++)
 {
-    string vowels = "aeiouy";
-    int count = 0;
-    for (int i = 0; i < str.Length; i++)
+    // string str = strArray[i]; попытка упростить запись строки ниже
+    int numString = StringToCharArray(strArray[i]).Length;
+    if (numString <= 3)
     {
-        for (int j = 0; j < vowels.Length; j++)
-        {
-            if (str[i] == vowels[j])
-            {
-                count++;
-            }
-        }
+        numThreeString++;
     }
-    return count;
 }
+// объявляем массив со строками длиной менее трех символов
+string[] strThreeArray = new string[numThreeString];
 
-// Пример 3 
-// строку Hello my world разбиваем на отдельные строки и ...(разворачиваем?)
-static string ReverseWord(string strNew)
+// заполняем массив strThreeArray
+int k = 0;
+for (int j = 0; j < strArray.Length; j++)
 {
-    string[] strNewSplit = strNew.Split(); // ["Hello", "my", "world"]
-
-    string res = "";
-
-    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    int numString = StringToCharArray(strArray[j]).Length;
+    if (numString <= 3)
     {
-        res += strNewSplit[i] + " ";
+        strThreeArray[k] = strArray[j];
+        k++;
     }
-
-    return res;
 }
 
-string strNew = "Hello my world";
-string res = ReverseWord(strNew);
-Console.WriteLine(res);
+// печать массива
 
+PrintArray(strArray);
+Console.WriteLine(); // Печать пустой строки
+PrintArray(strThreeArray);
+
+// Final
